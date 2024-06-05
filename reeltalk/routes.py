36f43563ef4210710bd8e3ec130fcb6e81a8c97b
Raj Dhadda <raj.dhadda@trescal.com>
2024-06-05 14:53:32 +0000

    feat: added search_movies function to use TMBD API to enable a user to search for movies

--- reeltalk/routes.py
+++ reeltalk/routes.py
@@ -1,6 +1,27 @@
+import os
+import requests
 from flask import render_template, request, redirect, url_for
 from reeltalk import app, db
+
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route('/search_movies', methods=['GET', 'POST'])
+def search_movies():
+    if request.method == 'POST':
+        search_query = request.form['search_query']
+        api_key = os.environ.get("API_KEY")
+        url = 'https://api.themoviedb.org/3/search/movie'
+        params = {'api_key': api_key, 'query': search_query}
+        response = requests.get(url, params=params)
+        
+        if response.status_code == 200:
+            data = response.json()
+            movies = data['results']
+            return render_template('search_results.html', movies=movies, search_query=search_query)
+        else:
+            return 'Error: Failed to fetch data from TMDb API'
+    return render_template('search_movies.html')
