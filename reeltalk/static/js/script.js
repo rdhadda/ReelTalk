5853005b3ab6a35d0dd039874d63386c14742a30
Raj Dhadda <raj.dhadda@trescal.com>
2024-06-06 12:39:15 +0000

    feat: added tooltip to view movie names longer than 30 characters

--- reeltalk/static/js/script.js
+++ reeltalk/static/js/script.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    
+    let tooltip = document.querySelectorAll('.tooltipped');
+    M.Tooltip.init(tooltip);
+    
   });
