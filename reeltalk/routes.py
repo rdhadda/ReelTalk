9b3d74962b7562c046e2fc64c638749894703d5f
Raj Dhadda <raj.dhadda@trescal.com>
2024-06-07 11:24:33 +0000

    feat: added error handling for movies with no search results

--- reeltalk/routes.py
+++ reeltalk/routes.py
@@ -1,6 +1,6 @@
 import os
 import requests
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, flash
 from reeltalk import app, db
 
 
@@ -15,15 +15,19 @@ def search_movies():
         search_query = request.form['search_query']
         api_key = os.environ.get("API_KEY")
         url = 'https://api.themoviedb.org/3/search/movie'
-        params = {'api_key': api_key, 'query': search_query,'include_adult': False }
+        params = {'api_key': api_key, 'query': search_query, 'include_adult': False}
         response = requests.get(url, params=params)
         
         if response.status_code == 200:
             data = response.json()
             movies = data['results']
+            if not movies: 
+                flash(f'No movies found for "{search_query}"', category='error')
+                return redirect(url_for('search_movies'))
             return render_template('search_results.html', movies=movies, search_query=search_query)
         else:
-            return 'Error: Failed to fetch data from TMDb API'
+            flash('Error: Failed to fetch movie details from TMDb API', category='error')
+            return redirect(url_for('search_movies'))
     return render_template('search_movies.html')
 
 @app.route('/movie/<int:movie_id>', methods=['GET'])
@@ -37,6 +41,7 @@ def movie_details(movie_id):
         movie = response.json()
         return render_template('review_form.html', movie=movie)
     else:
+        flash('Error: Failed to fetch movie details from TMDb API', category='error')
         return 'Error: Failed to fetch movie details from TMDb API'
 
 @app.route('/sign_up', methods=['GET', 'POST'])
