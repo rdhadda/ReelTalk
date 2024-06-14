a8c51f8b0c772fe878cfc92f1c83fadeb6d87768
Raj Dhadda <raj.dhadda@trescal.com>
2024-06-14 13:07:15 +0000

    feat: import flask login manager

--- reeltalk/__init__.py
+++ reeltalk/__init__.py
@@ -1,16 +1,24 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
 
 if os.path.exists("env.py"):
     import env  # noqa
-
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
-
 db = SQLAlchemy(app)
 
 from reeltalk import routes  # noqa
+
+login_manager = LoginManager()
+login_manager.login_view = "routes.home"
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
