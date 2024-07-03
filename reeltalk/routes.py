a41ab5a0eabceb40b4ed43afd2854287969879c4
Raj Dhadda <raj.dhadda@trescal.com>
2024-07-03 11:07:34 +0000

    comments: credited palletsprojects for 500 server error handling

--- reeltalk/routes.py
+++ reeltalk/routes.py
@@ -376,6 +376,7 @@ def page_not_found(e):
     return render_template('404.html')
 
 
+# 500 error handling taken from flask.palletsprojects
 @app.errorhandler(500)
 def internal_server_error(e):
     """
